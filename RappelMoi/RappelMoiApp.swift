2e63712051ef43f6c96fbddf64ca693122c30858
nin-faz <fazernino@gmail.com>
2025-08-08 11:41:18 +0200

    Initial Commit

---
+++ RappelMoi/RappelMoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RappelMoiApp.swift
+//  RappelMoi
+//
+//  Created by PRO BTP DIGITAL on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct RappelMoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RappelMoiTests/RappelMoiTests.swift
@@ -0,0 +1,17 @@
+//
+//  RappelMoiTests.swift
+//  RappelMoiTests
+//
+//  Created by PRO BTP DIGITAL on 07/08/2025.
+//
+
+import Testing
+@testable import RappelMoi
+
+struct RappelMoiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
